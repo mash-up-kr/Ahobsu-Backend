a99e9141bd88fb0aae1adf3696474d1b3ee4a97f
Yuni-Q <carnotcycle2@naver.com>
2019-11-22 02:00:20 +0900

    first commit

---
+++ src/main/kotlin/com/mahsup/ahoubsu/AhoubsuApplication.kt
@@ -0,0 +1,11 @@
+package com.mahsup.ahoubsu
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AhoubsuApplication
+
+fun main(args: Array<String>) {
+    runApplication<AhoubsuApplication>(*args)
+}
---
+++ src/test/kotlin/com/mahsup/ahoubsu/AhoubsuApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mahsup.ahoubsu
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AhoubsuApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
