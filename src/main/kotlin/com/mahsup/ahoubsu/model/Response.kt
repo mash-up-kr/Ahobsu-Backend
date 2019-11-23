46d3a081ca05ddd8cbbaa81e52c4b4150c3ae58a
Yuni-Q <carnotcycle2@naver.com>
2019-11-24 02:27:31 +0900

    fix: fix mission response
    fixes #6

--- src/main/kotlin/com/mahsup/ahoubsu/model/Response.kt
+++ src/main/kotlin/com/mahsup/ahoubsu/model/Response.kt
@@ -1,3 +1,3 @@
 package com.mahsup.ahoubsu.model
 
-data class Response<T>(val status: Int, val error: String? = null, val message: String, val data: T?)
+data class Response<T>(val status: Int, val error: String? = null, val message: String?, val data: T?)
